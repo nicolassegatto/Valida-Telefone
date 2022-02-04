a1456bff1664a60ea4fa2873cc82595e1bd0b840
Nicolas Segatto <nicolasssegatto@icloud.com>
2022-02-04 18:38:22 -0300

    criacao pagina de teste

--- validador.js
+++ validador.js
@@ -1,1 +1,14 @@
 //VALIDADOR DE TELEFONES.
+
+function VALIDADOR(telefone){
+
+//trata variável removendo qualquer caráctere ou letras
+//assim pode chegar variavel com () com letras, que vai extrari somente os numeros em sequencia para validação.
+    telefone = telefone.toString();
+    telefone = telefone.replace(/[^0-9]/g, '');
+    
+
+    return telefone
+}
+
+console.log(VALIDADOR("oi nicolas 996302407"))
